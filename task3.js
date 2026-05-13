5393bcfb319623dbfee8e7ec2b5f708e1a3860e1
DESKTOP-IP1R72P\Admin <poojahosmanihk@gmail.com>
2026-05-13 15:34:51 +0530

    Added Javascript Tasks

--- task3.js
+++ task3.js
@@ -1,9 +1,9 @@
 //Looping statment
 //1. Print numbers
-let a = 1
-for(let a=1; a<=20; a++){
-   console.log(a); 
-}
+// let a = 1
+// for(let a=1; a<=20; a++){
+//    console.log(a); 
+// }
 
 //2. Odd numbers
 // let i = 1
@@ -22,103 +22,103 @@ for(let a=1; a<=20; a++){
 // }
 
 //4. Reverse Counting
-let i = 20;
-while(i >= 1){
-    console.log(i);
-    i--;
-}
+// let i = 20;
+// while(i >= 1){
+//     console.log(i);
+//     i--;
+// }
 
 //5. Sum of numbers
-let sum = 0;
-for(let i = 1; i <= 100; i++){
-    sum += i;
-}
-console.log("Total sum =", sum);
-
-//6. Array Loop
-let fruits = ["apple", "banana","grapes","oranges"];
-for (let i = 0; i < fruits.length; i++){
-    console.log(fruits[i]); 
-}
-
-//7. Count Even numbers
-let count = 0;
-for (let i = 1; i <= 50; i++){
-    if (i % 2 == 0){
-        count++;
-    }
-}
-console.log("Total Even Numbers =", count); // Output : Total even numbers = 25
-
-//8. Star Pattern
-for (let i = 1; i <= 5; i++){
-    let stars = "";
-    for(let j = 1; j <= i; j++){
-        stars += "*";
-    }
-    console.log(stars);
-}
-
-//Function
-//9. Simple Function
-function welcome(){
-    console.log("Welcome to javascript!");
-}
-welcome() // Output : Welocome tp javascript
-
-//10. Function with Parameter
-function greet(name){
-    console.log("Hello" + name);
-}
-greet("Pooja") // Output : HelloPooja
-
-//11. Add Two Numbers
-function add(a,b){
-    return a+b;
-}
-console.log(add(10,20)); // Output : 30
-
-//12. Salary Bonus
-// function totalSalary(salary,bonus){
-//     return salary+bonus;
+// let sum = 0;
+// for(let i = 1; i <= 100; i++){
+//     sum += i;
 // }
-// console.log(totalSalary(50000,5000)); // Output : 55000
+// console.log("Total sum =", sum);
 
-// //13. Object Loop
-// let student ={
-//     name: "Rahul",
-//     course: "javascript",
-//     marks: 95
-// };
-// for (let key in student){
-//     console.log(key + ":" + student[key]);
+// //6. Array Loop
+// let fruits = ["apple", "banana","grapes","oranges"];
+// for (let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]); 
 // }
 
-//14. Find Largest Number
-function largest(a,b){
-    if(a>b){
-        return a;
-    }
-    else{
-        return b;
-    }
-}
-console.log(largest(10,50)); // Output : 50
+// //7. Count Even numbers
+// let count = 0;
+// for (let i = 1; i <= 50; i++){
+//     if (i % 2 == 0){
+//         count++;
+//     }
+// }
+// console.log("Total Even Numbers =", count); // Output : Total even numbers = 25
+
+// //8. Star Pattern
+// for (let i = 1; i <= 5; i++){
+//     let stars = "";
+//     for(let j = 1; j <= i; j++){
+//         stars += "*";
+//     }
+//     console.log(stars);
+// }
+
+// //Function
+// //9. Simple Function
+// function welcome(){
+//     console.log("Welcome to javascript!");
+// }
+// welcome() // Output : Welocome tp javascript
+
+// //10. Function with Parameter
+// function greet(name){
+//     console.log("Hello" + name);
+// }
+// greet("Pooja") // Output : HelloPooja
+
+// //11. Add Two Numbers
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(10,20)); // Output : 30
+
+// //12. Salary Bonus
+// // function totalSalary(salary,bonus){
+// //     return salary+bonus;
+// // }
+// // console.log(totalSalary(50000,5000)); // Output : 55000
+
+// // //13. Object Loop
+// // let student ={
+// //     name: "Rahul",
+// //     course: "javascript",
+// //     marks: 95
+// // };
+// // for (let key in student){
+// //     console.log(key + ":" + student[key]);
+// // }
+
+// //14. Find Largest Number
+// function largest(a,b){
+//     if(a>b){
+//         return a;
+//     }
+//     else{
+//         return b;
+//     }
+// }
+// console.log(largest(10,50)); // Output : 50
 
 //15. Employee Task
-let employee = {
-    name: "Pooja",
-    department: "Software Development",
-    salary: 40000
-};
-function addBonus(salary,bonus){
-    return salary+bonus;
-}
-let finalSalary =addBonus(employee.salary, 5000);
-console.log("Employee.name:", employee.name);
-console.log("Department:", employee.department);
-console.log("Salary:", employee.salary);
-console.log("Salary after bonus:", finalSalary);
+// let employee = {
+//     name: "Pooja",
+//     department: "Software Development",
+//     salary: 40000
+// };
+// function addBonus(salary,bonus){
+//     return salary+bonus;
+// }
+// let finalSalary =addBonus(employee.salary, 5000);
+// console.log("Employee.name:", employee.name);
+// console.log("Department:", employee.department);
+// console.log("Salary:", employee.salary);
+// console.log("Salary after bonus:", finalSalary);
 
 
 
